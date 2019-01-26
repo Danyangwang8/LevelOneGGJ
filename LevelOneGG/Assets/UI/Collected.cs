c6c5265e4c513142bd72cbdb8501b00deae80bef
Rasmus T. O. Pedersen <rasmuspedersen@Rasmuss-MacBook-Pro.local>
2019-01-26 13:34:47 +0100

    UI shit dump 1

---
+++ LevelOneGG/Assets/UI/Collected.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collected : MonoBehaviour
+{
+    public string[] items_Collected = {"BCap","Guitar","GBottle" };
+    private Collected_Enable[] ggl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.GetComponentInChildren<Collected_Enable>().onOpen();
+    }
+    public void opened() {
+        gameObject.GetComponentInChildren<Collected_Enable>();
+        foreach (Collected_Enable item in ggl)
+        {
+            ggl.onOpen();
+        }
+      }
+}
