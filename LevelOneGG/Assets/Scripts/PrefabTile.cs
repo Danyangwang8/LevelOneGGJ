295663bc1b8b185a21b8d4a331c2008f935b1ea8
LSandborgh <lukas.sandborgh@gmail.com>
2019-01-26 21:46:55 +0100

    Made the basetiles detect if they are blocked by landmarks.

--- LevelOneGG/Assets/Scripts/PrefabTile.cs
+++ LevelOneGG/Assets/Scripts/PrefabTile.cs
@@ -7,6 +7,8 @@ public class PrefabTile : UnityEngine.Tilemaps.TileBase
     public Sprite Sprite; //The sprite of tile in the palette
     public GameObject Prefab; //The gameobject to spawn
 
+    private GameObject m_gameObject;
+
 
     public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
     {
@@ -20,6 +22,16 @@ public class PrefabTile : UnityEngine.Tilemaps.TileBase
         // Streangly the position of gameobject starts at Left Bottom point of cell and not at it center
         // TODO need to add anchor points  (vertical and horisontal (left,centre,right)(top,centre,bottom))
         go.transform.position += new Vector3(100,100,0);
+        m_gameObject = go;
+        return true;
+    }
+
+    public bool TileIsLandmark()
+    {
+        if (m_gameObject.GetComponentInChildren<Landmark>() != null)
+        {
+            return false;
+        }
         return true;
     }
 }
