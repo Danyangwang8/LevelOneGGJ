574c698df188c40c98fee2d436f0e50af85a9ed3
LSandborgh <lukas.sandborgh@gmail.com>
2019-01-26 15:28:21 +0100

    Added Tilemaps and art assets for tiles.

---
+++ LevelOneGG/Assets/Scripts/Editor/CreateCustomTileEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+class CreateCustomTileEditor : EditorWindow
+{
+    [MenuItem("Window/CreateCustomTileEditor")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(CreateCustomTileEditor));
+    }
+
+    void OnGUI()
+    {
+        //EditorGUILayout
+    }
+}
