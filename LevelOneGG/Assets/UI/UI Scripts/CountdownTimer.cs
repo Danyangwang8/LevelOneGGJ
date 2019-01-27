2b4365960e169a89c372e1a0e05221a655f8002b
Rasmus T. O. Pedersen <rasmuspedersen@Rasmuss-MacBook-Pro.local>
2019-01-27 15:45:24 +0100

    sound

--- LevelOneGG/Assets/UI/UI Scripts/CountdownTimer.cs
+++ LevelOneGG/Assets/UI/UI Scripts/CountdownTimer.cs
@@ -8,10 +8,12 @@ public class CountdownTimer : MonoBehaviour
     public GameObject manager;
     public GameManager gM;
     public Text timer;
+    private AudioSource playerAud1;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerAud1 = gameObject.GetComponent<AudioSource>();
         gM = manager.GetComponent<GameManager>();
         timer = gameObject.GetComponentInChildren<Text>();
      }
@@ -19,6 +21,10 @@ public class CountdownTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(gM.m_exploreTimer == 0) 
+        {
+            playerAud1.Play();
+        }
         if (gM.m_exploreTimer >= 0)
         {
             timer.text = "time left: " + gM.m_exploreTimer.ToString("F2");
